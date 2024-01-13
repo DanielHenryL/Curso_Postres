ad3d9daca94f295d90a52b255500c596c96e6cad
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-12 21:46:11 -0500

    MAX,MIN,COUNT,ROUND,AVG(SACAR PROMEDIO ARITMETICO) y SUM

--- 04-Funciones-agrupacion-ordenamiento/02.sql
+++ 04-Funciones-agrupacion-ordenamiento/02.sql
@@ -10,3 +10,14 @@ where
 ORDER BY 
 	followers asc;
 	
+SELECT * FROM users;
+
+-- MAX,MIN,COUNT,ROUND,AVG(SACAR PROMEDIO ARITMETICO) y SUM,
+SELECT
+    COUNT(*) as total_users,
+    MIN( followers ) as min_followers,
+    MAX( followers ) as max_followers,
+    ROUND(AVG( followers )) as avg_followers,
+    SUM( followers ) / COUNT(*) as avg_manual 
+FROM
+    users;
