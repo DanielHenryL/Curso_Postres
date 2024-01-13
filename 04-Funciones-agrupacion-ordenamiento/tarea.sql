a11d68aae6090cf1a407182f26ee86f480e6851b
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-12 21:28:03 -0500

    Tarea para el calentamiento de la seccion

---
+++ 04-Funciones-agrupacion-ordenamiento/tarea.sql
@@ -0,0 +1,9 @@
+
+SELECT * from users;
+
+-- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XXX
+SELECT first_name, last_name, last_connection from users where last_connection like '221%';
+
+
+-- Nombre, apellido y seguidores(followers) de todos a los que lo siguen más de 4600 personas
+SELECT first_name, last_name, followers FROM users where followers > 4600;
