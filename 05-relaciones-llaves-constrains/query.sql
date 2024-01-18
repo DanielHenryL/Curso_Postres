2d13ef5847291c09563905fd9b5e8aadb03b6a16
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-18 16:28:31 -0500

    agregar llave primaria al campo "code"

---
+++ 05-relaciones-llaves-constrains/query.sql
@@ -0,0 +1,7 @@
+SELECT * from country;
+-- Agregar la primary key en el campo code
+ALTER TABLE country ADD PRIMARY KEY (code);
+-- Pretendemos hacer la eliminacion de registro y es recomendable primero hacer la consulta para estar seguro
+SELECT * from country where code='NLD' AND code2 ='NA';
+-- Eliminar el registro
+delete from country where code='NLD' AND code2 ='NA';
