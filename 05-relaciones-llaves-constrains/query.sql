16ae365bdc519ee50d388e61039c682ac1bd7e82
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-18 16:54:39 -0500

    Constraint - Check

--- 05-relaciones-llaves-constrains/query.sql
+++ 05-relaciones-llaves-constrains/query.sql
@@ -1,7 +1,16 @@
-SELECT * from country;
 -- Agregar la primary key en el campo code
 ALTER TABLE country ADD PRIMARY KEY (code);
 -- Pretendemos hacer la eliminacion de registro y es recomendable primero hacer la consulta para estar seguro
 SELECT * from country where code='NLD' AND code2 ='NA';
 -- Eliminar el registro
 delete from country where code='NLD' AND code2 ='NA';
+
+
+SELECT * from country;
+-- agregar check(condicion)
+ALTER TABLE country ADD CHECK(
+	surfacearea >= 0
+);
+
+
+
