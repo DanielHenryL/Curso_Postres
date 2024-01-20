90ea4df58bc22762bf54fb34c1069e9f6334f8b6
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-19 22:02:57 -0500

    Llave foreign key

--- 05-relaciones-llaves-constrains/query.sql
+++ 05-relaciones-llaves-constrains/query.sql
@@ -53,3 +53,11 @@ CREATE UNIQUE INDEX "unique_name_countrycode_district" on city(
 CREATE INDEX "index_district" on city(
 	district
 );
+
+
+-- llave FOREIGN KEY de la tabla city a la tabla country campo countrycode y code, respectivamente.
+ALTER TABLE city ADD CONSTRAINT fk_country_code FOREIGN KEY ( countrycode ) REFERENCES country ( code );
+
+--Insert un registro.
+INSERT INTO country
+		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
