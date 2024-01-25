782ee363619f28a6dbcf723f66c1826a0bd6d411
DanielHenryL <lagunasdaniel001@gmail.com>
2024-01-25 11:24:06 -0500

    Union de tablas con where

--- 07-Joins-Uniones/query.sql
+++ 07-Joins-Uniones/query.sql
@@ -3,3 +3,8 @@ SELECT code, name FROM continent WHERE name LIKE '%America'
 UNION
 SELECT code, name FROM continent WHERE code in (3,5)
 ORDER BY name ASC;
+
+-- Unir dos tablas con where
+SELECT a.name as pais, b.name as continent FROM country a, continent b
+WHERE a.continent = b.code
+ORDER BY b.name ASC;
